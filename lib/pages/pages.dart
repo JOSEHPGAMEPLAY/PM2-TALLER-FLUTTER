5af4d6ee52e7617a57268544074be3780e62e2fa
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-11-04 16:32:39 -0500

    Creacion de estructura del proyecto provisional

---
+++ lib/pages/pages.dart
@@ -0,0 +1,3 @@
+export 'package:pokemons/pages/home.dart';
+export 'package:pokemons/pages/pokemon.dart';
+export 'package:pokemons/pages/notFound.dart';
---
+++ lib/widgets/Widgets.dart
@@ -0,0 +1,4 @@
+export 'package:pokemons/widgets/AppBar.dart';
+export 'package:pokemons/widgets/Avatar.dart';
+export 'package:pokemons/widgets/ButtonDialog.dart';
+export 'package:pokemons/widgets/Dialog.dart';
