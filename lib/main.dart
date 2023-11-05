eb40121a99cf1d7b8572c50cbf68af74e8b2885b
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-11-04 20:26:17 -0500

    Se modifico el archivo main para trabajar con las rutas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pokemons/routes/routes.dart';
+import 'package:pokemons/theme/appTheme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,15 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme:AppTheme.darkTheme,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      initialRoute: Rutas.initialRoute,
+      routes: Rutas.routes,
+      onGenerateRoute: Rutas.onGernerate,
     );
   }
 }
