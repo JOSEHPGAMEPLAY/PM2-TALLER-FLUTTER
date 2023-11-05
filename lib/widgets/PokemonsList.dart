0da159a852b46e658bcae58c8822461e9c7a9ef2
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-11-04 22:16:33 -0500

    Se creo la lista

---
+++ lib/widgets/PokemonsList.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:pokemons/data/pokemonsMap.dart';
+
+class PokemonsList extends StatelessWidget {
+  const PokemonsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      separatorBuilder: (context, index) => const Divider(
+        color: Colors.grey,
+        height: 5,
+      ),
+      itemBuilder: (BuildContext context, index) {
+        index = index + 1;
+
+        String avatar = pokemonsMap[index]?['avatar'];
+        String nombre = pokemonsMap[index]?['nombre'];
+        String descripcion = pokemonsMap[index]?['descripcion'];
+
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.black26,
+            foregroundImage: AssetImage(avatar), //
+          ),
+          title: Text(nombre),
+          subtitle: Text(descripcion),
+          trailing: const Icon(
+            Icons.arrow_right,
+            size: 40,
+          ),
+          onLongPress: () {},
+        );
+      },
+      itemCount: pokemonsMap.length,
+    );
+  }
+}
