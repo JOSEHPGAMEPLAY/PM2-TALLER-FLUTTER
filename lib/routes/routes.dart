56eb5c591fb2492925cb4b3ea828b95387908733
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-11-05 10:55:45 -0500

    Modificamos el archivo de rutas para pasarle informacion a la pagina de pokemon

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -5,7 +5,17 @@ class Rutas {
   static const initialRoute = '/home';
   static Map<String, Widget Function(BuildContext)> routes = {
     '/home': (context) => const Home(),
-    '/pokemon': (context) => const Pokemon(),
+    '/pokemon': (context) {
+      final Map<String, dynamic>? args =
+          ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+      if (args != null) {
+        final Map<String, dynamic> pokemonu = args['pokemonu'];
+        return Pokemon(pokemonu: pokemonu);
+      }
+      else{
+        return const Home();
+      }
+    },
   };
   static Route<dynamic> onGernerate(RouteSettings settings) {
     return MaterialPageRoute(
