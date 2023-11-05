d24e6a959439eefbd78367081e493bb6f2e78007
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-11-04 19:18:52 -0500

    Se renombro el archivo pokemosList a pokemosMap, se lleno el mismo archivo, se edito el archivo yaml se habilitaron los assets

---
+++ lib/data/pokemonsMap.dart
@@ -0,0 +1,37 @@
+Map<int, Map<String,dynamic>> pokemonsMap = {
+  1: {
+    'avatar':'assets/Pikachu.png',
+    'nombre': 'Pikachu',
+    'descripcion': 'Ratón eléctrico',
+    'informacion': 'Pikachu es un Pokémon tipo eléctrico muy conocido por su capacidad de generar electricidad. Es la mascota de la franquicia Pokémon.',
+    'habilidades': ['Impactrueno', 'Rayo', 'Trueno', 'Electrocañón']
+  },
+  2: {
+    'avatar':'assets/Charizard.png',
+    'nombre': 'Charizard',
+    'descripcion': 'Dragón de fuego',
+    'informacion': 'Charizard es un Pokémon tipo fuego/volador. Es la evolución final de Charmander y es conocido por su aliento de fuego.',
+    'habilidades': ['Lanzallamas', 'Garra dragón', 'Gruñido', 'Vuelo']
+  },
+  3: {
+    'avatar':'assets/Bulbasaur.png',
+    'nombre': 'Bulbasaur',
+    'descripcion': 'Planta semilla',
+    'informacion': 'Bulbasaur es un Pokémon tipo planta/veneno. Tiene una semilla en su lomo que crece a medida que evoluciona.',
+    'habilidades': ['Látigo cepa', 'Gruñido', 'Drenadoras', 'Rayo solar']
+  },
+  4: {
+    'avatar':'assets/Squirtle.png',
+    'nombre': 'Squirtle',
+    'descripcion': 'Tortuga de agua',
+    'informacion': 'Squirtle es un Pokémon tipo agua. Puede disparar agua a alta presión desde su boca y es uno de los iniciales de Kanto.',
+    'habilidades': ['Pistola agua', 'Gruñido', 'Surf', 'Hidrobomba']
+  },
+  5: {
+    'avatar':'assets/Eevee.png',
+    'nombre': 'Eevee',
+    'descripcion': 'Evolución múltiple',
+    'informacion': 'Eevee es un Pokémon único que puede evolucionar en varias formas diferentes, incluyendo Vaporeon, Jolteon, Flareon y más.',
+    'habilidades': ['Rizo defensa', 'Bola sombra', 'Deseo', 'Doble filo']
+  }
+};
